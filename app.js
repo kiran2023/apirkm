239e04281e91b87d5d4d2b33ac79a77db7e7f487
KIRAN <88279441+kiran2023@users.noreply.github.com>
2024-01-27 19:42:54 +0530

    Updated Authentication

--- app.js
+++ app.js
@@ -1,26 +1,74 @@
-const express = require('express');
-const app = express();
-const productsRouter = require('./routes/productsRoutes');
-const authRouter = require('./routes/authRoutes')
-const customError = require('./utils/customError');
-const errorController = require('./controllers/errorController');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const path = require("path");
 
+const helmet = require("helmet");
+const sanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const rateLimit = require("express-rate-limit");
+
+const productsRouter = require("./routes/productsRoutes");
+const authRouter = require("./routes/authRoutes");
+const customError = require("./utils/customError");
+const errorController = require("./controllers/errorController");
+
+const app = express();
+const limiter = rateLimit({
+  max: 10,
+  windowMs: 60 * 60 * 1000,
+  message: "Too Many Requests from this IP, Please Try Again in an Hour!",
+});
+
+app.use(helmet());
+
 var corsOptions = {
-    origin: 'https://rkmapi-production.up.railway.app/',
-    methods: 'GET, POST, PUT, PATCH, DELETE, HEAD',
-    credentials: true,
-}
+  origin: "https://rkmapi-production.up.railway.app/",
+  methods: "GET, POST, PUT, PATCH, DELETE, HEAD",
+  credentials: true,
+};
 app.use(cors(corsOptions));
-app.use(express.json()); 
 
-app.use('/', productsRouter);
-app.use('/api/v1', authRouter);
+app.use(limiter);
+app.use(express.json({ limit: "10kb" }));
+app.use(sanitize());
+app.use(xss());
+app.use(
+  hpp({
+    whitelist: [
+      "Stock",
+      "category",
+      "uniqueId",
+      "filterValue",
+      "value",
+      "image",
+      "alt",
+      "title",
+      "productName",
+      "quantity",
+      "rating",
+      "ratingAverage",
+      "originalPrice",
+      "currentPrice",
+      "discountPercentage",
+    ],
+  })
+);
 
-app.all("*", (request, response, next)=>{
-    const error = new customError(`Page Not Found for the Requested URL ${request.url}`, 404); 
-    next(error);
+app.use("/", express.static(path.join(__dirname, "./public/mainPage")));
+app.use(
+  "/products",
+  express.static(path.join(__dirname, "./public/products/"))
+);
+app.use("/api/v1/products", productsRouter);
+app.use("/api/v1", authRouter);
+
+app.all("*", (request, response, next) => {
+  const error = new customError(
+    `Page Not Found for the Requested URL ${request.url}`,
+    404
+  );
+  next(error);
 });
 
 app.use(errorController);
