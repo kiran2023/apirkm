4522da5f80e4209907507d57c3c7fb02fb2f3724
KIRAN <88279441+kiran2023@users.noreply.github.com>
2024-01-30 21:08:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -55,13 +55,14 @@ app.use(
   })
 );
 
+app.use("/", productsRouter);
+app.use("/api/v1", authRouter);
 app.use("/", express.static(path.join(__dirname, "./public/mainPage")));
 app.use(
   "/products",
   express.static(path.join(__dirname, "./public/products/"))
 );
-app.use("/api/v1/products", productsRouter);
-app.use("/api/v1", authRouter);
+
 
 app.all("*", (request, response, next) => {
   const error = new customError(
